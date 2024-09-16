0f15cdc8a59943fabba6a929e842140b20e73847
ericlempe <ericlempe1994@gmail.com>
2024-09-16 22:27:26 +0100

    add logout test

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Livewire\Auth\Login;
+use App\Livewire\Auth\Logout;
 use App\Livewire\Auth\Register;
 use App\Livewire\Welcome;
 use Illuminate\Support\Facades\Route;
@@ -10,7 +11,7 @@ Route::get('/', Welcome::class);
 
 Route::get('/login', Login::class)->name('login');
 Route::get('/register', Register::class)->name('auth.register');
-Route::get('/logout', fn() => auth()->logout())->name('auth.logout');
+Route::get('/logout', Logout::class)->name('auth.logout');
 
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', function () {})->name('dashboard');
