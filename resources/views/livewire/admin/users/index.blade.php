5d9652cca9f161cb18d55b1befa709a238037588
ericlempe <ericlempe1994@gmail.com>
2024-10-18 11:42:41 +0100

    add filter in the table of users

--- resources/views/livewire/admin/users/index.blade.php
+++ resources/views/livewire/admin/users/index.blade.php
@@ -1,6 +1,18 @@
 <div>
     <x-header title="Users" separator/>
 
+    <div class="flex flex-column flex-md-row justify-between mb-4">
+        <div class="w-1/3">
+            <x-input
+                placeholder="Search by name and email"
+                class="input-sm"
+                icon="o-magnifying-glass"
+                wire:model.live.debounce="search"
+            />
+        </div>
+        <div></div>
+    </div>
+
     <x-table :headers="$this->headers" :rows="$this->users" with-pagination>
         @scope('cell_permissions', $user)
         @foreach($user->permissions as $permission)
