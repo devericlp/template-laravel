8868540c3023120d296e64fa554ea1c512d82ac7
ericlempe <ericlempe1994@gmail.com>
2025-01-14 22:45:22 -0300

    it should send that new code to the user by email

--- tests/Feature/Auth/EmailVerificationTest.php
+++ tests/Feature/Auth/EmailVerificationTest.php
@@ -2,9 +2,16 @@
 
 use App\Listeners\Auth\CreateValidationCode;
 use App\Models\User;
+use App\Notifications\ValidateCodeNotification;
 use Illuminate\Auth\Events\Registered;
 
+use Illuminate\Support\Facades\Notification;
+
 use function PHPUnit\Framework\assertTrue;
+
+beforeEach(function () {
+    Notification::fake();
+});
 
 it('should create a new validation code and save in the users table', function () {
 
@@ -17,15 +24,22 @@ it('should create a new validation code and save in the users table', function (
     $user->refresh();
 
     expect($user->validation_code)->not->toBeNull()
-    ->and($user)->validation_code->toBeNumeric();
+        ->and($user)->validation_code->toBeNumeric();
 
     assertTrue(str($user->validation_code)->length() === 6);
 
 });
 
 it('should send that new code to the user by email', function () {
+    $user = User::factory()->create(['email_verified_at' => null, 'validation_code' => null]);
 
-})->todo();
+    $event    = new Registered($user);
+    $listener = new CreateValidationCode();
+    $listener->handle($event);
+
+    Notification::assertSentTo([$user], ValidateCodeNotification::class);
+
+});
 
 test('making sure that the listener to send the code linked to the registered event', function () {
 
